112434bcd62d590f959d06c74ca1be6def3a42ca
Gurpreet Sudan <gurpreetsudan2094@gmail.com>
2021-03-03 15:07:53 +0530

    bottom navigation working added navigation library

--- app/src/main/java/com/gurpreet/singh/app/HomeActivity.kt
+++ app/src/main/java/com/gurpreet/singh/app/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.gurpreet.singh.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +12,10 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
